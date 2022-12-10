7b5c83c26a3293bf4b04b2d8357c1d7a96712c52
gitJanaeW <janae.wel@gmail.com>
2022-12-10 17:21:25 -0500

    testing webpack + tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, {Suspense, lazy} from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-// import Nav from "./components/Nav";
+import Nav from "./components/Nav.js";
 // const About = lazy(() => import('./components/About'));
 
 const App = () => {
     return(
         <BrowserRouter>
-            {/* <Nav/> */}
+            <Nav/>
             <h1 className="bg-red-900 text-yellow">Hi, I'm React, Webpack</h1>
                 {/* <Suspense fallback={<div style={{minHeight:"10vh"}}></div>}>
                     <Routes>
