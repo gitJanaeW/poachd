fa5ec815627ef7500d3f3e7f2f6e39d4ced0e100
gitJanaeW <janae.wel@gmail.com>
2022-12-12 09:45:57 -0500

    added home route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, {Suspense, lazy} from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Nav from "./components/Nav.js";
+const Landing = lazy(() => import("./pages/Landing.js"));
 const Footer = lazy(() => import("./components/Footer.js"));
-// const About = lazy(() => import('./components/About'));
 
 const App = () => {
     return(
         <BrowserRouter>
             <Nav/>
-                {/* <Suspense fallback={<div style={{minHeight:"10vh"}}></div>}>
+                <Suspense fallback={<div style={{minHeight:"10vh"}}></div>}>
                     <Routes>
-                        <Route path="/" element={<About/>}/>
+                        <Route path="/" element={<Landing/>}/>
                     </Routes>
-                </Suspense> */}
+                </Suspense>
             <Footer/>
         </BrowserRouter>
     );
